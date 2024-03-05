309658cfc01ec15bbe7e99b7a3d55a238a313c0e
Vinicius Bastos <vinicius.bastos@me.com>
2024-03-04 23:24:20 -0300

    third

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-
+import config from './config'
 import app from './server'
 
 // creates and starts a server for our API on a defined port
-app.listen(3001, () => {
+app.listen(config.port, () => {
   console.log(`Example app listening at http://localhost:3001`)
 })
