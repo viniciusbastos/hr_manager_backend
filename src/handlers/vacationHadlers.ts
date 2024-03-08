c9ad08eaecc65783ce41cea949150178f5c756ae
Vinicius Bastos <vinicius.bastos@me.com>
2024-03-08 04:27:50 -0300

    order by vacation list mont1

--- src/handlers/vacationHadlers.ts
+++ src/handlers/vacationHadlers.ts
@@ -2,27 +2,7 @@ import { comparePasswords, createJWT, hashPassword } from './../modules/auth'
 import prisma from '../db'
 import { Request, Response } from 'express'
 
-export const createUser = async (req: any, res: any, next: any) => {
-  try {
-    const hash = await hashPassword(req.body.password)
 
-    const user = await prisma.user.create({
-      data: {
-        name: req.body.name,
-        password: hash,
-        posto: req.body.posto,
-        mat: req.body.mat,
-        username: req.body.username,
-        email: req.body.email
-      }
-    })
-    const token = createJWT(user)
-    res.json({ token: token })
-  } catch (e: any) {
-    e.type = 'input'
-    next(e)
-  }
-}
 
 
 
