08bb5379cb00e3f7958e9e1922a2dbddec8e794d
Vinicius <vinicius.bastos@me.com>
2025-01-08 09:57:18 -0300

    fjfj

--- src/routes/vacations.routes.ts
+++ src/routes/vacations.routes.ts
@@ -32,9 +32,10 @@ vacationRouter.get('/vacation/:id', async (req, res) => {
 })
 vacationRouter.get('/vacations/month/:month/:year', async (req, res) => {
   const { month } = req.params
+  const { year } = req.params
   const intMonth = parseInt(month)
   const vacation: any =
-    await prisma.$queryRaw`SELECT "Vacation".id, "User"."name", "User".posto, "User".mat, "Vacation"."month","Vacation"."year", "Vacation"."period", "Vacation"."startAt", "Vacation"."finishAt" FROM "User" INNER join "Vacation" ON "User".id = "Vacation"."belongsToId" WHERE "Vacation"."month" = ${intMonth} and "Vacation"."year" = '2025'   ORDER BY "User".posto asc  `
+    await prisma.$queryRaw`SELECT "Vacation".id, "User"."name", "User".posto, "User".mat, "Vacation"."month","Vacation"."year", "Vacation"."period", "Vacation"."startAt", "Vacation"."finishAt" FROM "User" INNER join "Vacation" ON "User".id = "Vacation"."belongsToId" WHERE "Vacation"."month" = ${intMonth} and "Vacation"."year" = ${year}   ORDER BY "User".posto asc  `
   res.json(vacation)
 })
 
